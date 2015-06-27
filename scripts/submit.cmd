da7222a609343de4b816bbd6faa5f88623976008
Jörn Dinkla <joern@dinkla.net>
2015-06-27 11:46:41 +0200

    hadoop 2.6

--- scripts/submit.cmd
+++ scripts/submit.cmd
@@ -1,2 +1,2 @@
 rem call in client mode
-spark-submit.cmd --class net.dinkla.lbnn.Main --master spark://v1:7077 --deploy-mode client target/scala-2.10/lbnn-assembly-1.0.jar cluster.properties %1 %2 %3
+spark-submit.cmd --class net.dinkla.lbnn.Main --master spark://v1:7077 --conf "spark.executor.extraJavaOptions=-Djava.library.path=/home/ubuntu/hadoop/lib/native" --deploy-mode client target/scala-2.10/lbnn-assembly-1.0.jar cluster.properties %1 %2 %3
