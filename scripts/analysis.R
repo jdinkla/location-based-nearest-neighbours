2592ef0adbf0b7a364a2f818d943e41d00085a59
Jörn Dinkla <joern@dinkla.net>
2015-07-02 12:32:51 +0200

    Locations Map improved

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -2,9 +2,12 @@
 #
 # R code for some diagrams for the lbnn app
 # 
+# Installation instructions:
 #
-# $ install.packages(c("ggplot2"))
-# 
+# This file depends on the packages ggplot2 and ggmap.
+#
+# $ install.packages(c("ggplot2", "ggmap"))
+#
 # for pretty fonts the following extra package is needed
 #
 # $ install.packages("extrafont")
@@ -14,27 +17,17 @@
 # $ library(extrafont)
 # $ font_import()
 #
-# but warning: this may take a while 
+# but warning: this may take a while
+#
 # see http://blog.revolutionanalytics.com/2012/09/how-to-use-your-favorite-fonts-in-r-charts.html
 # and http://www.r-bloggers.com/change-fonts-in-ggplot2-and-create-xkcd-style-graphs/
 #
-# maps:
-# install.packages(c("maps"))
-# install.packages(c("ggmap"))
-
-setwd("C:/workspace/location-based-nearest-neighbours/temp")
-
-library(ggplot2)
-library(extrafont)
-library(ggmap)
-
-# To cite ggmap in publications, please use:
-
+# ---------------------------------------------------------------------
+#
+# ggmap was published in
 #   D. Kahle and H. Wickham. ggmap: Spatial Visualization with ggplot2. The R Journal,
 #   5(1), 144-161. URL http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf
-
-# A BibTeX entry for LaTeX users is
-
+#
 #   @Article{,
 #     author = {David Kahle and Hadley Wickham},
 #     title = {ggmap: Spatial Visualization with ggplot2},
@@ -46,13 +39,25 @@ library(ggmap)
 #     url = {http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf},
 #   }
 
+setwd("C:/workspace/location-based-nearest-neighbours/temp")
+
+use_hdfs <- FALSE
+
+library(ggplot2)
+library(ggmap)
+library(extrafont)
+
+if (use_hdfs) {
+    library(rhdfs)
+}
+
 lbnn_init <- function() {
 #font_import()
 	loadfonts()
 #	loadfonts(device="win")
 }
 
-use_hdfs <- FALSE
+
 	# if (use_hdfs) {
 	# 	rs <- NULL
 	# } else {
@@ -69,6 +74,9 @@ dinkla_blue <- "#1E2D5B"
 dinkla_dark_blue <- "#0F162E"
 dinkla_red <- "#AF0A14"
 font_family <- "Verdana"
+
+export_width <- 3820
+export_height <- 2160
 
 #
 # auxiliary functions 
@@ -109,6 +117,10 @@ mk_palette <- function(color, n = 10, alpha_start = 0.1, alpha_end = 0.9) {
 	mapply(f, alphas)
 }
 
+export <- function(filename) {
+  png(filename, width=export_width, height=export_height, units="px")
+}
+
 #
 # sums_hh: checkins per hour 
 #
@@ -117,13 +129,24 @@ mk_palette <- function(color, n = 10, alpha_start = 0.1, alpha_end = 0.9) {
 
 sums_hh <- read_textkey_value(file="sums_hh.csv")
 
-chart_hh <- function() {
+chart_hh <- function(scale=1) {
+  
+  # here pie is used, not a ggplot2 function
+  size_cex <- 1 * scale
+  size_cex_main <- 2.5 * scale
+
 	vs <- sums_hh$value
 	cs <- mk_palette(dinkla_blue, length(vs), 0.5, 0.95)
 	ls <- sums_hh$hh
 	pct <- round(vs/sum(vs)*100)
 	ls <- paste(paste(ls, pct, sep="\n"), "%", sep="")
-	pie(vs, col=cs, labels=ls, clockwise=TRUE, border=NULL, main="Number of checkins during the day", family="Verdana", col.main=dinkla_red, col.lab=dinkla_dark_blue, cex=1.34, cex.main=3)
+	pie(vs, col=cs, labels=ls, clockwise=TRUE, border=NULL, main="Number of checkins during the day", family="Verdana", col.main=dinkla_red, col.lab=dinkla_dark_blue, cex=size_cex, cex.main=size_cex_main, mai=c(15, 4, 4, 2) + 10.1, lheight=10)
+}
+
+export_hh <- function() {
+  export("lbnn_piechart.png")
+  chart_hh(2.15)
+  dev.off()
 }
 
 #
@@ -146,7 +169,8 @@ chart_ym <- function(scale = 1) {
 			theme(
 				panel.background = element_rect(fill=mk_color(dinkla_blue, 0.1)),
 				panel.grid.major.x = element_blank(),
-    		    panel.grid.minor.x = element_blank()) + 
+    		    panel.grid.minor.x = element_blank()
+    		) +
 			ggtitle("Number of check-ins per month in the loc-gowalla dataset") +
    			xlab("Month") + ylab("Number of check-ins")  +
 			theme(
@@ -237,84 +261,36 @@ chart_ns <- function(binwidth = 8, scale = 1) {
 }
 
 #
+# sums_ns: Number of neighbors
 #
+# displayed on a map
 #
+
 sums_loc <- read.csv(file="sums_location.csv", header=TRUE, sep=";")
 
-# define the charts
+chart_loc <- function(scale = 1) {
 
-chart_loc <- function() {
-
+  size_small <- 12 * scale
+  size_middle <- 16 * scale
+  size_large <- 24 * scale
+  
 	# calc percentages
 	a <- sums_loc
 	a$pct <- a$number.of.checkins/sum(a$number.of.checkins)*100
 
 	# filter data
 	a <- a[a$pct >= 0.1,]
-
-	#w <- get_map("USA", zoom=4, maptype="satellite")
-	w <- get_map("USA", zoom=4)
+	# get the map and add points
+  w <- get_map("Salina, Kansas, USA", zoom=4, scale=2)
 	p <- ggmap(w) +
-			ggtitle("Regions with a high number of check-ins") + 
+	  ggtitle("Regions with a high number of check-ins") + 
 			theme(
-				plot.title=element_text(size=size_large, color=dinkla_red, family=font_family)
+				plot.title=element_text(size=size_large, color=dinkla_red, family=font_family),
+				legend.position="top",
+				legend.title=element_text(size=size_middle, color=dinkla_blue, family=font_family),
+				legend.text=element_text(size=size_small, color=dinkla_blue, family=font_family)
 			)
 
 	p + geom_point(data=a, aes(locationY, locationX, color=pct), size=10, alpha=.9) + 
-			scale_color_gradient2(low=dinkla_dark_blue, mid=dinkla_blue, high=dinkla_red)
+			scale_color_gradient2(low=dinkla_dark_blue, mid=dinkla_blue, high=dinkla_red, name="in %")
 }
-
-
-hide <- function() {
-
-	1
-# 	states_map <- map_data("state")
-# 	ggplot(states_map, aes(x=long, y=lat, group=group)) + geom_polygon(fill="white", colour="black")
-
-# w_map <- map_data("world")
-
-# p + geom_point(data=sums_loc, aes(locationY, locationX, color=pct), size=8, alpha=.9) + scale_color_gradient2(high=dinkla_red)
-# p <- ggplot(w_map, aes(x=long, y=lat, group=group)) + geom_polygon(fill="white", colour="black")
-# a <- sums_loc[sums_loc$pct >= 0.005,]
-# p + geom_point(data=d, aes(lat, lon))
-# p + geom_point(data=a, aes(locationY, locationX), color=dinkla_red, size=5, alpha=.9) 
-
-# p + geom_point(data=a, aes(locationY, locationX, color=pct), size=10, alpha=.9) + scale_color_gradient2(high=dinkla_red)
-
-
-# p + geom_point(data=d, aes(lon, lat), size=15, color="red", alpha=.5)
-#  sums_loc$pct <- sums_loc$number.of.checkins/sum(sums_loc$number.of.checkins)
-
-
-# qplot(sums_ym$yyyymm, sums_ym$value, geom="line")
-# qplot(sums_ym$yyyymm, sums_ym$value, geom=c("line", "point"))
-# qplot(sums_ym$yyyymm, sums_ym$value, geom="bar", stat="identity")
-# ggplot(sums_ym, aes(x=yyyymm, y=value)) + geom_bar(stat="identity", fill=dinkla_blue, color="black")
-
-# qplot(sums_ymd$yyyymm, sums_ymd$value, geom="line")
-# qplot(sums_ymd$yyyymm, sums_ymd$value, geom=c("line", "point"))
-# ggplot(sums_ymd, aes(x=yyyymmdd, y=value)) + geom_line(color=dinkla_blue)
-
-# hist(sums_ns1$number.of.neighbors)
-# hist(sums_ns1$number.of.neighbors, breaks=25)
-# qplot(sums_ns1$number.of.neighbors, binwidth=2)
-
-# b <- sums_ymd
-# b$type <- rep("Check-ins", length(sums_ymd$value))
-# c <- sums_ymd
-# c$value <- smooth(sums_ymd$value)
-# c$type <- rep("Smoothed", length(c$value))
-# d <- rbind(b, c)
-# ggplot(d, aes(x=yyyymmdd, y=value, color=type)) + geom_line() + scale_color_manual(values=c(dinkla_blue, dinkla_red))
-# ggplot(sums_ymd, aes(x=yyyymmdd, y=value)) + geom_area(fill=dinkla_blue, alpha=.3) + geom_line(color=dinkla_blue)
-
-# # sums_ym
-# ggplot(sums_ym, aes(x=yyyymm, y=value)) + geom_bar(stat="identity", fill=dinkla_blue)
-
-# #ggsave("font_ggplot.pdf", plot=p,  width=16, height=9)
-# ## needed for Windows - make sure YOU have the correct path for your machine:
-# #Sys.setenv(R_GSCMD = "C:\\Program Files (x86)\\gs\\gs9.06\\bin\\gswin32c.exe")
-# #embed_fonts("font_ggplot.pdf")
-
-}
-
